5c46d2366774bb81ef21b20a1c8e9d1d8a485612
Ritom Gupta <ritomgupta99@gmail.com>
2021-11-10 23:05:43 +0530

    revert but no revert

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,20 @@
-module.exports = ({
-  basePath = "/",
-  contentPath = "content/",
-  showThemeLogo = true,
-  theme = "dark-green",
-}) => {
-  return {
-    siteMetadata: {
-      description: "Personal page of Ritom Gupta",
-      locale: "en",
-      showThemeLogo:false,
-      title: "Ritom Gupta",
-      formspreeEndpoint: "https://formspree.io/f/xnqlkdlv",
+module.exports = {
+  siteMetadata: {
+    description: "Personal page of Ritom Gupta",
+    locale: "en",
+    title: "Ritom Gupta",
+    formspreeEndpoint: "https://formspree.io/f/xnqlkdlv",
+  },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        basePath: "/",
+        contentPath: "content/",
+        showThemeLogo: false,
+        theme: "dark-green",
+      },
     },
-    plugins: [
-      {
-        resolve: `gatsby-plugin-postcss`,
-        options: {
-          postCssPlugins: [
-            require("tailwindcss")(require("./tailwind.config")(theme)),
-            require("postcss-input-range"),
-            require("autoprefixer"),
-          ],
-        },
-      },
-      `gatsby-plugin-react-helmet`,
-      `gatsby-transformer-yaml`,
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          path: contentPath,
-        },
-      },
-      `gatsby-plugin-react-svg`,
-      `gatsby-plugin-image`,
-      `gatsby-transformer-sharp`,
-      `gatsby-plugin-sharp`,
-    ],
-  }
+    `gatsby-plugin-sharp`
+  ],
 }
