f8e88f7ac3d0338133c0e59098eaa3866c0b3b5c
Ritom Gupta <ritomgupta99@gmail.com>
2021-11-10 22:59:49 +0530

    no work yet

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,41 +1,44 @@
-module.exports = {
-  siteMetadata: {
-    description: "Personal page of Ritom Gupta",
-    locale: "en",
-    title: "Ritom Gupta",
-    formspreeEndpoint: "https://formspree.io/f/xnqlkdlv",
-  },
-  plugins: [
-    {
-      resolve: "@wkocjan/gatsby-theme-intro",
-      options: {
-        basePath: "/",
-        contentPath: "content/",
-        showThemeLogo: false,
-        theme: "dark-green",
-      },
+module.exports = ({
+  basePath = "/",
+  contentPath = "content/",
+  showThemeLogo = true,
+  theme = "dark-green",
+}) => {
+  return {
+    siteMetadata: {
+      description: "Personal page of Ritom Gupta",
+      locale: "en",
+      showThemeLogo:false,
+      title: "Ritom Gupta",
+      formspreeEndpoint: "https://formspree.io/f/xnqlkdlv",
     },
-    {
-      resolve: `gatsby-plugin-postcss`,
-      options: {
-        postCssPlugins: [
-          require("tailwindcss")(require("./tailwind.config")(theme)),
-          require("postcss-input-range"),
-          require("autoprefixer"),
-        ],
+    plugins: [
+      {
+        resolve: `gatsby-plugin-postcss`,
+        options: {
+          postCssPlugins: [
+            require("tailwindcss")(require("./tailwind.config")(theme)),
+            require("postcss-input-range"),
+            require("autoprefixer"),
+          ],
+        },
       },
-    },
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-yaml`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: contentPath,
+      `gatsby-plugin-react-helmet`,
+      `gatsby-transformer-yaml`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          path: contentPath,
+        },
       },
-    },
-    `gatsby-plugin-react-svg`,
-    `gatsby-plugin-image`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-  ],
+      `gatsby-plugin-react-svg`,
+      `gatsby-plugin-image`,
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+    ],
+  }
 }
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
