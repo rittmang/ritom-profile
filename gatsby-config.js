1296fe0ad78d280e84ac2279f54cc22a77e88926
Ritom Gupta <ritomgupta99@gmail.com>
2021-11-10 21:37:35 +0530

    ritom-profile-gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    description: "Personal page of Ritom Gupta",
+    locale: "en",
+    title: "Ritom Gupta",
+    formspreeEndpoint: "https://formspree.io/f/xnqlkdlv",
+  },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        basePath: "/",
+        contentPath: "content/",
+        showThemeLogo: false,
+        theme: "dark-green",
+      },
+    },
+  ],
+}
